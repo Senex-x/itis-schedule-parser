f97dba6322357ea8f313033e32add897715bad5a
V-Senex <junior.07.inator@gmail.com>
2022-03-17 19:15:46 +0300

    Move files

---
+++ src/main/java/com/itis/timetable/parser/MainPageController.kt
@@ -0,0 +1,48 @@
+package com.itis.timetable.parser
+
+import com.itis.timetable.data.models.group.Group
+import com.itis.timetable.data.repositories.GroupRepository
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.stereotype.Component
+import org.springframework.stereotype.Controller
+import org.springframework.web.bind.annotation.GetMapping
+
+@Controller
+class MainPageController {
+
+    @Autowired
+    lateinit var tester: Tester
+
+    @GetMapping("/")
+    fun getMainPage(): String {
+        tester.test()
+        return "main_page"
+    }
+}
+
+@Component
+class Tester {
+    @Autowired
+    private lateinit var repo: GroupRepository
+
+    fun test() {
+        repo.save(Group())
+
+        print(repo.get(1))
+    }
+}
+
+@Configuration
+open class Config {
+    @Bean
+    open fun provideGroupRepository() = GroupRepository()
+}
+
+
+
+
+
+
+
