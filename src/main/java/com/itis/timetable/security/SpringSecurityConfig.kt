37b6e0e2b49cbc25539354049e712070e69f68f9
V-Senex <vdm.snx@gmail.com>
2022-05-21 11:44:08 +0300

    Enable Spring Boot security

---
+++ src/main/java/com/itis/timetable/security/SpringSecurityConfig.kt
@@ -0,0 +1,12 @@
+package com.itis.timetable.security
+
+import org.springframework.context.annotation.Configuration
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter
+
+
+@Configuration
+open class SpringSecurityConfig: WebSecurityConfigurerAdapter() {
+    fun configure() {
+
+    }
+}
