c7a15d99f33c484dcd6cb05e42699a0928e1a076
V-Senex <junior.07.inator@gmail.com>
2023-03-01 14:00:51 +0300

    Rely on JDBC auto configuration

--- src/main/java/com/itis/timetable/di/HibernateConfiguration.kt
+++ src/main/java/com/itis/timetable/di/HibernateConfiguration.kt
@@ -32,9 +32,9 @@ open class HibernateConfiguration {
 
     @Bean
     open fun provideDataSource() = DriverManagerDataSource().apply {
-        Class.forName("org.postgresql.Driver").newInstance()
+        //Class.forName("org.postgresql.Driver").newInstance()
         //setDriverClassName(DriverManager.drivers().findFirst().get().javaClass.name)
-        setDriverClassName(environment.getProperty("spring.datasource.driver-class-name")!!)
+        //setDriverClassName(environment.getProperty("spring.datasource.driver-class-name")!!)
         url = environment.getProperty("spring.datasource.url")
         username = environment.getProperty("spring.datasource.username")
         password = environment.getProperty("spring.datasource.password")
